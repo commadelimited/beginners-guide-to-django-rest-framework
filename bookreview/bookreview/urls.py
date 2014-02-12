e91bf01d33989ad89195ec5737fb647752cd8cd8
andy matthews <andy@commadelimited.com>
2014-02-11 23:55:21 -0600

    Add Author model

--- bookreview/bookreview/urls.py
+++ bookreview/bookreview/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'bookreview.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
--- bookreview/books/models.py
+++ bookreview/books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.utils.timezone import now
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        default=now(),
+        editable=False,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        default=now(),
+        editable=False,
+    )
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('books_author_detail', (), {'pk': self.pk})
