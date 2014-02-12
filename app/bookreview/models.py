9ef1ef18bded18978ca5177bacc29609c1c809b9
andy matthews <andy@commadelimited.com>
2014-02-12 11:39:13 -0600

    Remove unneeded fields in Author model

--- app/bookreview/models.py
+++ app/bookreview/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from django.conf import settings
-from django.utils.timezone import now
 
 class Author(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    created_at = models.DateTimeField( auto_now_add=True, default=now(), editable=False)
-    updated_at = models.DateTimeField( auto_now_add=True, default=now(), editable=False)
