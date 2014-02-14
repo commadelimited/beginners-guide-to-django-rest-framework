aaab961f4e06e31bc3097612e1c6064446855201
andy matthews <andy@commadelimited.com>
2014-02-13 23:37:45 -0600

    Add index route/view. Add dummy data

--- app/bookreview/urls.py
+++ app/bookreview/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, url
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from bookreview import views
+from bookreview.views import index_view, AuthorView
 
 urlpatterns = patterns(
     '',
-
-    # Ping endpoint, which clients can hit periodically to trigger any processing tasks
-    url(r'^authors/$', views.AuthorView.as_view(), name='author-view'),
+    url(r'^$', index_view, name='index_view'),
+    url(r'^authors/$', AuthorView.as_view(), name='author-view'),
 
 )
 
--- app/bookreview/views.py
+++ app/bookreview/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import (
+    render,
+)
+
 from rest_framework import generics
 
 from bookreview.models import (
@@ -6,6 +10,16 @@ from bookreview.models import (
 from bookreview.serializers import (
     AuthorSerializer,
 )
+
+def index_view(request):
+    """
+    Ensure the user can only see their own profiles.
+    """
+    response = {
+        'authors': Author.objects.all()
+    }
+
+    return render(request, 'index.html', response)
 
 class AuthorView(generics.ListAPIView):
     """
